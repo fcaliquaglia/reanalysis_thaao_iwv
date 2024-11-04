d5234ab9016b86048a6db5d510ee787f3898b87e
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2024-11-04 10:27:06 +0100

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -40,7 +40,7 @@ basefol_out = os.path.join('H:\\Shared drives', 'Dati_elab_docs', 'thaao_compari
 tres = '24h'
 tres_rs = '1h'  # only for radiosoundings
 var_list = [
-    'iwv']  # , 'temp', 'rh', 'iwv', 'sw_down', 'sw_up', 'lw_up', 'lw_down', 'alb', 'lwp', 'cbh', 'precip', 'windd', 'winds', 'surf_pres']
+    'alb']  # , 'temp', 'rh', 'iwv', 'sw_down', 'sw_up', 'lw_up', 'lw_down', 'alb', 'lwp', 'cbh', 'precip', 'windd', 'winds', 'surf_pres']
 # 'tcc'
 
 years = np.arange(2016, 2025, 1)
